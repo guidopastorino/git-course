5fa37322774488ced719e491de91321f36fef5ab
Guido Pastorino <guidopasto05@gmail.com>
2024-03-02 11:16:32 -0300

    actualizando archivo C con matríz dinámica

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    int num = 10;
+    int **matriz = (int**)malloc(sizeof(int*) * 10);
+    for(int i = 0; i<10; i++){
+        matriz[i] = (int*)malloc(sizeof(int) * 10);
+    }
+
+
+    for(int i = 0; i<10; i++){
+        for(int j = 0; j<10; j++){
+            matriz[i][j] = 0;
+        }
+    }
+
+    for(int i = 0; i<10; i++){
+        for(int j = 0; j<10; j++){
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    for(int i = 0; i<10; i++){
+        for(int j = 0; j<10; j++){
+            free(matriz[i]);
+        }
+    }
+    free(matriz);
 
     return 0;
 }
