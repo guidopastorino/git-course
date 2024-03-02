59c60af5fe887f7818ad32d0a166ca9bca29ec1e
Guido Pastorino <96697892+guidopastorino@users.noreply.github.com>
2024-03-02 12:10:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,32 +2,7 @@
 #include <stdlib.h>
 
 int main(){
-    int **matriz = (int**)malloc(sizeof(int*) * 10);
-    for(int i = 0; i<10; i++){
-        matriz[i] = (int*)malloc(sizeof(int) * 10);
-    }
-
-
-    for(int i = 0; i<10; i++){
-        for(int j = 0; j<10; j++){
-            matriz[i][j] = 0;
-        }
-    }
-
-    for(int i = 0; i<10; i++){
-        for(int j = 0; j<10; j++){
-            printf("%d ", matriz[i][j]);
-        }
-        printf("\n");
-    }
-
-
-    for(int i = 0; i<10; i++){
-        for(int j = 0; j<10; j++){
-            free(matriz[i]);
-        }
-    }
-    free(matriz);
+    printf("No hay matriz\n");
 
     return 0;
 }
